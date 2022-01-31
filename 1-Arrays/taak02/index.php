b88f84f5797163d6cf0d390f71d4b0b68bb91fe7
Jurgen Louvris <jurgen.louvris@student.rocva.nl>
2022-01-31 11:52:39 +0100

    opdr

---
+++ 1-Arrays/taak02/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+
+<?php
+
+
+$persoon["voornaam"] = "Willem";
+$persoon["achternaam"] = "Alexander";
+$persoon["leeftijd"] = 53;
+$persoon["opleiding"] = "software ontwikkeling";
+
+echo $persoon['voornaam']. " ". $persoon['achternaam'] . " is " . $persoon['leeftijd']. " jaar oud en is student aan het ROC van Amsterdam aan de opleiding " . $persoon['opleiding'] ; 
+
+?>
+
+
+</body>
+</html>
