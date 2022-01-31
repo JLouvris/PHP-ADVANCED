a84426e64c3a6d53f6ae8aae4039d1cc46c5e961
Jurgen Louvris <jurgen.louvris@student.rocva.nl>
2022-01-31 10:39:13 +0100

    opdr

---
+++ 1-Arrays/taak01/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+
+<?php
+
+$gegevens = array ("voornaam" => "Jurgen", "Achternaam" => "Louvris", "Leeftijd" => "17", "klas" => "1C");
+echo "Gegevens van Yordany:";
+echo "<br>";
+echo "Voornaam : ". $gegevens["voornaam"];
+echo "<br>";
+echo "Achternaam : ". $gegevens["Achternaam"];
+echo "<br>";
+echo "Leeftijd : " . $gegevens["Leeftijd"]. " jaar " ;
+echo "<br>";
+echo "Klas : ", $gegevens["klas"];
+?>
+
+</body>
+</html>
