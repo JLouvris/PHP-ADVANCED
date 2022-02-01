13c1c0d736c1c778e26eb37dfd4adbf548d2310a
Jurgen Louvris <jurgen.louvris@student.rocva.nl>
2022-02-01 12:30:14 +0100

    opdr

---
+++ 2-Loops/taak02/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+
+<?php
+
+for($b = 0; $b <= 18; $b++ ){
+    echo '<ul>';
+    echo  "<li>ik ben". " " . $b. "jaar, dus ik mag nog niet stemmen</li>";
+    echo '</ul>';
+}
+echo  "ik ben 18 jaar oud dus ik heb stemrecht!";
+
+?>
+
+</body>
+</html>
