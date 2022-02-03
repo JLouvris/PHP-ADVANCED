c24ac27dde16e880f45a4643bf4b69c861be3291
Jurgen Louvris <jurgen.louvris@student.rocva.nl>
2022-02-03 17:47:22 +0100

    opdr

--- 2-Loops/taak01/index.php
+++ 2-Loops/taak01/index.php
@@ -5,10 +5,11 @@
 $leeftijd = 0;
 
 while( $leeftijd < 18){
-echo "ik ben". " " .$leeftijd. " jaar, ". " " ." dus ik nog niet stemmen,<br>";
+echo "ik ben". " " .$leeftijd. " jaar, dus ik mag nog niet stemmen,<br>";
 $leeftijd++;
 
 }
+echo "ik ben 18 jaar oud, dus ik mag nu stemmen <br>";
 
 ?>
 </body>
