0005c069f2f0a2dbd7dae0b993a3c04b7e92312f
Jurgen Louvris <jurgen.louvris@student.rocva.nl>
2022-02-07 12:14:15 +0100

    opdr

---
+++ 3-Forms/taak01/gegevens.php
@@ -0,0 +1,13 @@
+<!DOCTYPE HTML>
+<html>  
+<body>
+Voornaam is: <?php  echo $_POST["voornaam"]; ?> <br>
+Achternaam :<?php echo $_POST["achternaam"];?><br>
+Klas: <?php echo $_POST["klas"];?> <br>
+Leeftijd: <?php echo $_POST["leeftijd"];?> <br>
+Adres: <?php echo$_POST["adres"];  ?> <br>
+Plaatsnaam :<?php echo $_POST["plaatsnaam"];?> <br>
+Favoriete muziekband: <?php echo $_POST["muziek_band"];?> <br>
+
+</body>
+</html> 
