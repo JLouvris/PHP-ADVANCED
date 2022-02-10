a67316150da1fd48fa6ee0738fc1c99d142cc46c
Jurgen Louvris <jurgen.louvris@student.rocva.nl>
2022-02-10 11:53:39 +0100

    opdr

---
+++ 4-Functions/taak01/functies-1.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<body>
+
+<?php function schrijfKleur( $kleuren ){
+   echo "<p style='color:$kleuren'>Deze tekst is in de kleur$kleuren</p>";
+} 
+
+schrijfKleur('red');
+schrijfKleur('orange');
+schrijfKleur('purple');
+schrijfKleur('gold');
+
+?>
+
+
+</body>
+
+</html>
