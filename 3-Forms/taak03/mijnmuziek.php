5a373f0cbcfbe1210067a91b3cc029c4beda7560
Jurgen Louvris <jurgen.louvris@student.rocva.nl>
2022-02-10 11:28:10 +0100

    opdr

---
+++ 3-Forms/taak03/mijnmuziek.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<body>
+<?php
+
+echo "<ul>";
+echo "<li>Artiest is ".$_GET["artiest"];
+echo "</li><li>Titel is ".$_GET["titel"];
+echo "</li><li>Jaar van uitkomst is ".$_GET["jaar"];
+echo "</li><li>Muzieksoort is ".$_GET["muzieksoort"]."</li>";
+echo "</ul>";
+?>
+
+</body>
+</html> 
